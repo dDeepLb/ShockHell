20a2e34e53975f809c32a97b5ff59510516f3f74
dDeepLb <ddeeplb@gmail.com>
2024-09-26 07:04:46 +0300

    add first triggers for shock

---
+++ PiShockEventsTriggers.cs
@@ -0,0 +1,16 @@
+﻿namespace ShockHell {
+  class ConditionModuleTriggers : PlayerConditionModule {
+    public override void OnTakeDamage(DamageInfo info) {
+      base.OnTakeDamage(info);
+      if (info.m_Blocked) return;
+      float damage = info.m_Damage;
+
+      if (m_HP > 0) {
+        PiShockManager.Shock((int) damage, 1);
+      } else {
+        PiShockManager.Shock(100, 1);
+        PiShockManager.Shock(50, 3);
+      }
+    }
+  }
+}
