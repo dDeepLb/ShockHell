1d45c2ca3e467d6e52f5b1112f6f9ca92cd78df3
dDeepLb <ddeeplb@gmail.com>
2024-09-26 07:03:55 +0300

    add methods to control shocker

--- PiShockManager.cs
+++ PiShockManager.cs
@@ -1,4 +1,5 @@
-﻿using System.IO;
+﻿using Newtonsoft.Json;
+using System.IO;
 using UnityEngine;
 
 namespace ShockHell {
@@ -7,7 +8,24 @@ namespace ShockHell {
     public static string APIKey { get; set; }
     public static string Code { get; set; }
     private static readonly SimpleConfig Config = new SimpleConfig(Path.Combine(Application.persistentDataPath, "ShockHell.cfg"));
+    private static readonly string ApiUrl = "https://do.pishock.com/api/apioperate/";
 
+    public static void Shock(int intensity, int duration) {
+      intensity = Mathf.Clamp(intensity, 1, 100);
+      duration = Mathf.Clamp(duration, 1, 15);
+
+      ModAPI.Log.Write($"Shock! for {duration} seconds at {intensity} power");
+
+      SendPiShockRequest(PiShockOperations.Shock, intensity, duration);
+    }
+
+    public static void Vibrate(int intensity, int duration) {
+      SendPiShockRequest(PiShockOperations.Vibrate, intensity, duration);
+    }
+
+    public static void Beep(int duration) {
+      SendPiShockRequest(PiShockOperations.Beep, 0, duration);
+    }
 
     public static void SaveAuthConfig() {
       ModAPI.Log.Write("Saving PiShock Auth");
@@ -22,6 +40,24 @@ namespace ShockHell {
       Username = Config.GetValue("Auth", "Username", "");
       APIKey = Config.GetValue("Auth", "APIKey", "");
       Code = Config.GetValue("Auth", "Code", "");
+    }
+
+    private static string CreatePiShockPayload(PiShockOperations operation, int intensity, int duration) {
+      PiShockRequestModel pishockRequest = new PiShockRequestModel() {
+        Username = Username,
+        Apikey = APIKey,
+        Code = Code,
+        Name = "ShockHell",
+        Intensity = intensity,
+        Duration = duration,
+        Op = operation
+      };
+      return JsonConvert.SerializeObject(pishockRequest);
+    }
+
+    private static void SendPiShockRequest(PiShockOperations operation, int intensity, int duration) {
+      string payload = CreatePiShockPayload(operation, intensity, duration);
+      SimpleHttpClient.Post(ApiUrl, payload);
     }
   }
 
